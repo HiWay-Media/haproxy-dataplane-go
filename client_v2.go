22654e0c4efa3214da580aa876431176683c7552
OmarAouini <30733260+OmarAouini@users.noreply.github.com>
2022-05-19 14:41:07 +0200

    init

---
+++ client_v2.go
@@ -0,0 +1,378 @@
+/*
+HAPROXY DATAPLANE API CLIENT (V2)
+AUTHOR: Omar Aouini
+mail: aouini.omar93@gmail.com
+Date: 04/19/2022
+*/
+
+package haproxy
+
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
+// the Haproxy client type Interface
+type IHaproxyClient interface {
+	GetBasicInfo() (*HaproxyInfo, error)
+	GetVersion() (*string, error) // get haproxy version
+	GetSites() (*HaproxySites, error)
+	GetStats() (*HaproxyStats, error)
+	GetReloads() (*HaproxyReloads, error)
+	GetTransactions() (*HaproxyTransactions, error)
+	GetConfigurationGlobal() (*HaproxyConfigurationGlobal, error)
+	GetConfigurationDefaults() (*HaproxyConfigurationDefaults, error)
+	GetBackends() (*HaproxyBackends, error)
+	GetFrontends() (*HaproxyFrontends, error)
+	GetBackendSwitchingRules(frontend string) (*HaproxyBackendSwitchingRules, error)
+	GetServers(backend string) (*HaproxyFrontends, error)
+	GetAcls(parentType string, parentName string) (*HaproxyAcls, error) //parentType eg: "backend" or "frontend"
+	GetServerSwitchingRules(backend string) (*HaproxyServerSwitchingRules, error)
+	GetHttpRequestRules(parentType string, parentName string) (*HaproxyHttpRequestRules, error)
+	AddBackend(transactionId string, backend *HaproxyAddBackend) error
+	AddFrontend(transactionId string, addFrontend *HaproxyAddFrontend) error
+	AddAcl(parenttype string, parentName string, transactionId string, addAcl *HaproxyAddAcl) error
+	AddServer(backend string, transactionId string, addServer *HaproxyAddServer) error
+	AddHttpRequestRule(parentType string, parentName string, transactionId string, addRule *HaproxyAddHttpRequestRule) error
+	AddBackendSwitchingRule(frontend string, transactionId string, addRule *HaproxyAddBackendSwitchingRule) error
+	StartTransaction(haproxyVersion string) (*string, error)
+	CommitTransaction(transactionId string) error
+	CheckDuplicateDefinitions() (*HaproxyDuplicateDefinitionsResult, error) // check for duplicate definitions in the haproxy cfg
+}
+
+type haproxyClient struct {
+	// url eg: http://10.11.10.25:5555
+	Url  string
+	Rest *resty.Client
+}
+
+//create a new Haproxy client
+//
+//the debug argument is used in case you want to print the request for debug purposes.
+//
+// example usage:
+//
+// client := haproxy.NewHaproxyClient("http://127.0.0.1", "user", "password", false)
+func NewHaproxyClient(haproxyUrl string, basicAuthUsername string, basicAuthPassword string, debug bool) IHaproxyClient {
+	client := haproxyClient{
+		Url:  haproxyUrl,
+		Rest: resty.New().SetBasicAuth(basicAuthUsername, basicAuthPassword),
+	}
+	if debug {
+		client.Rest.SetDebug(true)
+	}
+	return &client
+}
+
+func (h *haproxyClient) GetBasicInfo() (*HaproxyInfo, error) {
+	url := h.Url + "/v2/services/haproxy/info"
+	var response HaproxyInfo
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetVersion() (*string, error) {
+	url := h.Url + "/v2/services/haproxy/info"
+	var response HaproxyInfo
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response.Haproxy.Version, nil
+}
+
+func (h *haproxyClient) GetSites() (*HaproxySites, error) {
+	url := h.Url + "/v2/services/haproxy/sites"
+	var response HaproxySites
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetStats() (*HaproxyStats, error) {
+	url := h.Url + "/v2/services/haproxy/sites"
+	var response HaproxyStats
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetReloads() (*HaproxyReloads, error) {
+	url := h.Url + "/v2/services/haproxy/reloads"
+	var response HaproxyReloads
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetTransactions() (*HaproxyTransactions, error) {
+	url := h.Url + "/v2/services/haproxy/transactions"
+	var response HaproxyTransactions
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) StartTransaction(haproxyVersion string) (*string, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/transactions?version=%s", haproxyVersion)
+	var response HaproxyTransaction
+	_, err := h.Rest.R().SetResult(&response).Post(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response.ID, nil
+}
+
+func (h *haproxyClient) GetConfigurationGlobal() (*HaproxyConfigurationGlobal, error) {
+	url := h.Url + "/v2/services/haproxy/configuration/global"
+	var response HaproxyConfigurationGlobal
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetConfigurationDefaults() (*HaproxyConfigurationDefaults, error) {
+	url := h.Url + "/v2/services/haproxy/configuration/defaults"
+	var response HaproxyConfigurationDefaults
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetBackends() (*HaproxyBackends, error) {
+	url := h.Url + "/v2/services/haproxy/configuration/backends"
+	var response HaproxyBackends
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetFrontends() (*HaproxyFrontends, error) {
+	url := h.Url + "/v2/services/haproxy/configuration/frontends"
+	var response HaproxyFrontends
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetBackendSwitchingRules(frontend string) (*HaproxyBackendSwitchingRules, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/backend_switching_rules?frontend=%s", frontend)
+	var response HaproxyBackendSwitchingRules
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetServers(backend string) (*HaproxyFrontends, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/servers?backend=%s", backend)
+	var response HaproxyFrontends
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+//parent type: backend or frontend
+func (h *haproxyClient) GetAcls(parentType string, parentName string) (*HaproxyAcls, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/acls?parent_type=%s&parent_name=%s", parentType, parentName)
+	var response HaproxyAcls
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+	return &response, nil
+}
+
+func (h *haproxyClient) GetServerSwitchingRules(backend string) (*HaproxyServerSwitchingRules, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/server_switching_rules?backend=%s", backend)
+	var response HaproxyServerSwitchingRules
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+
+	return &response, nil
+}
+
+func (h *haproxyClient) GetHttpRequestRules(parentType string, parentName string) (*HaproxyHttpRequestRules, error) {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/http_request_rules?parent_type=%s&parent_name=%s", parentType, parentName)
+	var response HaproxyHttpRequestRules
+	_, err := h.Rest.R().SetResult(&response).Get(url)
+	if err != nil {
+		return nil, err.(*HaproxyErrorResponse)
+	}
+
+	return &response, nil
+}
+
+func (h *haproxyClient) AddFrontend(transactionId string, addFrontend *HaproxyAddFrontend) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/frontends?transaction_id=%s", transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddFrontend{}).SetBody(addFrontend).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) AddBackend(transactionId string, addBackend *HaproxyAddBackend) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/backends?transaction_id=%s", transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddBackend{}).SetBody(addBackend).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) AddAcl(parenttype string, parentName string, transactionId string, addAcl *HaproxyAddAcl) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/acls?parent_type=%s&parent_name=%s&transaction_id=%s", parenttype, parentName, transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddAcl{}).SetBody(addAcl).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) AddServer(backend string, transactionId string, addServer *HaproxyAddServer) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/servers?backend=%s&transaction_id=%s", backend, transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddServer{}).SetBody(addServer).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) AddHttpRequestRule(parentType string, parentName string, transactionId string, addRule *HaproxyAddHttpRequestRule) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/http_request_rules?parent_type=%s&parent_name=%s&transaction_id=%s", parentType, parentName, transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddHttpRequestRule{}).SetBody(addRule).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) AddBackendSwitchingRule(frontend string, transactionId string, addRule *HaproxyAddBackendSwitchingRule) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/configuration/backend_switching_rules?frontend=%s&transaction_id=%s", frontend, transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyAddBackendSwitchingRule{}).SetBody(addRule).Post(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) CommitTransaction(transactionId string) error {
+	url := h.Url + fmt.Sprintf("/v2/services/haproxy/transactions/%s", transactionId)
+	_, err := h.Rest.R().SetResult(&HaproxyCommitTransaction{}).Put(url)
+	if err != nil {
+		return err.(*HaproxyErrorResponse)
+	}
+
+	return nil
+}
+
+func (h *haproxyClient) CheckDuplicateDefinitions() (*HaproxyDuplicateDefinitionsResult, error) {
+	result := HaproxyDuplicateDefinitionsResult{}
+
+	// //BACKENDS
+	// backends, err := h.GetBackends()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// backendsNames := []string{}
+	// for _, v := range backends.Data {
+	// 	backendsNames = append(backendsNames, v.Name)
+	// }
+	// result.Backends = dupesCheck(backendsNames)
+
+	// //FRONTENDS
+	// frontends, err := h.GetFrontends()
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// frontendNames := []string{}
+	// for _, v := range frontends.Data {
+	// 	frontendNames = append(frontendNames, v.Name)
+	// }
+	// result.Frontends = dupesCheck(frontendNames)
+
+	// //ACLS
+	//FOR EACH BACKEND and FRONTEND
+	// acls, err := h.GetAcls("backend", "gpu-f-c0-1")
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// aclsnames := []string{}
+	// for _, v := range acls.Data {
+	// 	aclsnames = append(aclsnames, v.AclName)
+	// }
+	// result.Acls = dupesCheck(aclsnames)
+
+	//SERVERS
+	//FOR EACH BACKEND
+	// servers, err := h.GetServers("gpu-f-c0-1")
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// serversNames := []string{}
+	// for _, v := range servers.Data {
+	// 	serversNames = append(serversNames, v.Name)
+	// }
+	// result.Servers = dupesCheck(serversNames)
+
+	return &result, nil
+}
+
+func dupesCheck(list []string) []DuplicateCount {
+	duplicate_frequency := make(map[string]int)
+	result := []DuplicateCount{}
+
+	for _, item := range list {
+		// check if the item/element exist in the duplicate_frequency map
+		_, exist := duplicate_frequency[item]
+
+		if exist {
+			duplicate_frequency[item] += 1 // increase counter by 1 if already in the map
+		} else {
+			duplicate_frequency[item] = 1 // else start counting from 1
+		}
+	}
+
+	for k, v := range duplicate_frequency {
+		if v > 1 { //only add duplicate to result (more than 1 count)
+			res := DuplicateCount{}
+			res.Name = k
+			res.Count = v
+			result = append(result, res)
+		}
+	}
+
+	return result
+}
---
+++ models.go
@@ -0,0 +1,350 @@
+/*
+HAPROXY DATAPLANE API CLIENT (V2)
+AUTHOR: Omar Aouini
+mail: aouini.omar93@gmail.com
+Date: 04/19/2022
+*/
+
+package haproxy
+
+type HaproxyErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (h *HaproxyErrorResponse) Error() string {
+	return h.Message
+}
+
+type HaproxyInfo struct {
+	Haproxy struct {
+		Pid         int    `json:"pid"`
+		Processes   int    `json:"processes"`
+		ReleaseDate string `json:"release_date"`
+		Uptime      int    `json:"uptime"`
+		Version     string `json:"version"`
+	} `json:"haproxy"`
+}
+
+type HaproxySites struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Farms []struct {
+			Balance struct {
+				Algorithm string `json:"algorithm"`
+			} `json:"balance"`
+			Mode    string `json:"mode"`
+			Name    string `json:"name"`
+			Servers []struct {
+				Address        string      `json:"address"`
+				Check          string      `json:"check"`
+				Inter          int         `json:"inter"`
+				Maxconn        int         `json:"maxconn"`
+				Name           string      `json:"name"`
+				Port           int         `json:"port"`
+				ProxyV2Options interface{} `json:"proxy-v2-options"`
+				Weight         int         `json:"weight"`
+			} `json:"servers"`
+			UseAs string `json:"use_as"`
+		} `json:"farms"`
+		Name    string `json:"name"`
+		Service struct {
+			Listeners []struct {
+				Name    string `json:"name"`
+				Address string `json:"address"`
+				Port    int    `json:"port"`
+			} `json:"listeners"`
+			Mode string `json:"mode"`
+		} `json:"service"`
+	} `json:"data"`
+}
+
+type HaproxyStats []struct {
+	Error      string `json:"error"`
+	RuntimeAPI string `json:"runtimeAPI"`
+	Stats      []struct {
+		BackendName string `json:"backend_name"`
+		Name        string `json:"name"`
+		Stats       struct {
+			Bin         int    `json:"bin"`
+			Bout        int    `json:"bout"`
+			CompByp     int    `json:"comp_byp"`
+			CompIn      int    `json:"comp_in"`
+			CompOut     int    `json:"comp_out"`
+			CompRsp     int    `json:"comp_rsp"`
+			ConnRate    int    `json:"conn_rate"`
+			ConnRateMax int    `json:"conn_rate_max"`
+			ConnTot     int    `json:"conn_tot"`
+			Dcon        int    `json:"dcon"`
+			Dreq        int    `json:"dreq"`
+			Dresp       int    `json:"dresp"`
+			Dses        int    `json:"dses"`
+			Ereq        int    `json:"ereq"`
+			Hrsp1Xx     int    `json:"hrsp_1xx"`
+			Hrsp2Xx     int    `json:"hrsp_2xx"`
+			Hrsp3Xx     int    `json:"hrsp_3xx"`
+			Hrsp4Xx     int    `json:"hrsp_4xx"`
+			Hrsp5Xx     int    `json:"hrsp_5xx"`
+			HrspOther   int    `json:"hrsp_other"`
+			Iid         int    `json:"iid"`
+			Intercepted int    `json:"intercepted"`
+			Mode        string `json:"mode"`
+			Pid         int    `json:"pid"`
+			Rate        int    `json:"rate"`
+			RateLim     int    `json:"rate_lim"`
+			RateMax     int    `json:"rate_max"`
+			ReqRate     int    `json:"req_rate"`
+			ReqRateMax  int    `json:"req_rate_max"`
+			ReqTotal    int    `json:"req_total"`
+			Scur        int    `json:"scur"`
+			Slim        int    `json:"slim"`
+			Smax        int    `json:"smax"`
+			Status      string `json:"status"`
+			Stot        int    `json:"stot"`
+		} `json:"stats"`
+		Type string `json:"type"`
+	} `json:"stats"`
+}
+type HaproxyReloads []struct {
+	ID     string `json:"id"`
+	Status string `json:"status"`
+}
+
+type HaproxyTransactions []struct {
+	Version int    `json:"_version"`
+	ID      string `json:"id"`
+	Status  string `json:"status"`
+}
+
+type HaproxyTransaction struct {
+	Version int    `json:"_version"`
+	ID      string `json:"id"`
+	Status  string `json:"status"`
+}
+
+type HaproxyConfigurationGlobal struct {
+	Version int `json:"_version"`
+	Data    struct {
+		CPUMaps []struct {
+			CPUSet  string `json:"cpu_set"`
+			Process string `json:"process"`
+		} `json:"cpu_maps"`
+		RuntimeApis []struct {
+			Address string `json:"address"`
+			Level   string `json:"level"`
+			Mode    string `json:"mode"`
+		} `json:"runtime_apis"`
+		Daemon                string `json:"daemon"`
+		MasterWorker          bool   `json:"master-worker"`
+		Maxconn               int    `json:"maxconn"`
+		Nbproc                int    `json:"nbproc"`
+		SslDefaultBindCiphers string `json:"ssl_default_bind_ciphers"`
+		SslDefaultBindOptions string `json:"ssl_default_bind_options"`
+		StatsTimeout          int    `json:"stats_timeout"`
+		TuneSslDefaultDhParam int    `json:"tune_ssl_default_dh_param"`
+	} `json:"data"`
+}
+
+type HaproxyConfigurationDefaults struct {
+	Version int `json:"_version"`
+	Data    struct {
+		ErrorFiles []struct {
+			Code int    `json:"code"`
+			File string `json:"file"`
+		} `json:"error_files"`
+		Balance struct {
+			Algorithm string        `json:"algorithm"`
+			Arguments []interface{} `json:"arguments"`
+		} `json:"balance"`
+		ClientTimeout  int `json:"client_timeout"`
+		ConnectTimeout int `json:"connect_timeout"`
+		DefaultServer  struct {
+		} `json:"default_server"`
+		Dontlognull          string `json:"dontlognull"`
+		HTTPKeepAliveTimeout int    `json:"http_keep_alive_timeout"`
+		Httplog              bool   `json:"httplog"`
+		Mode                 string `json:"mode"`
+		QueueTimeout         int    `json:"queue_timeout"`
+		Redispatch           struct {
+			Enabled string `json:"enabled"`
+		} `json:"redispatch"`
+		ServerTimeout int `json:"server_timeout"`
+	} `json:"data"`
+}
+
+type HaproxyBackends struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Balance struct {
+			Algorithm string        `json:"algorithm"`
+			Arguments []interface{} `json:"arguments"`
+		} `json:"balance,omitempty"`
+		Mode       string `json:"mode"`
+		Name       string `json:"name"`
+		Forwardfor struct {
+			Enabled string `json:"enabled"`
+		} `json:"forwardfor,omitempty"`
+		Httpchk struct {
+			Method string `json:"method"`
+			URI    string `json:"uri"`
+		} `json:"httpchk,omitempty"`
+		HTTPConnectionMode string `json:"http_connection_mode,omitempty"`
+		StickTable         struct {
+			Expire int    `json:"expire"`
+			Size   int    `json:"size"`
+			Store  string `json:"store"`
+			Type   string `json:"type"`
+		} `json:"stick_table,omitempty"`
+		AdvCheck string `json:"adv_check,omitempty"`
+		HashType struct {
+			Method string `json:"method"`
+		} `json:"hash_type,omitempty"`
+		HTTPKeepAliveTimeout int `json:"http_keep_alive_timeout,omitempty"`
+		HTTPRequestTimeout   int `json:"http_request_timeout,omitempty"`
+		ServerTimeout        int `json:"server_timeout,omitempty"`
+	} `json:"data"`
+}
+
+type HaproxyFrontends struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		DefaultBackend string `json:"default_backend,omitempty"`
+		Mode           string `json:"mode"`
+		Name           string `json:"name"`
+		Tcplog         bool   `json:"tcplog,omitempty"`
+		Forwardfor     struct {
+			Enabled string `json:"enabled"`
+		} `json:"forwardfor,omitempty"`
+	} `json:"data"`
+}
+
+type HaproxyBackendSwitchingRules struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Cond     string `json:"cond"`
+		CondTest string `json:"cond_test"`
+		Index    int    `json:"index"`
+		Name     string `json:"name"`
+	} `json:"data"`
+}
+
+type HaproxyServers struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Address string `json:"address"`
+		Check   string `json:"check"`
+		Name    string `json:"name"`
+		Port    int    `json:"port"`
+		Weight  int    `json:"weight"`
+	} `json:"data"`
+}
+
+type HaproxyAcls struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		AclName   string `json:"acl_name"`
+		Criterion string `json:"criterion"`
+		Index     int    `json:"index"`
+		Value     string `json:"value"`
+	} `json:"data"`
+}
+
+type HaproxyServerSwitchingRules struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Cond         string `json:"cond"`
+		CondTest     string `json:"cond_test"`
+		Index        int    `json:"index"`
+		TargetServer string `json:"target_server"`
+		ReturnHdrs   string `json:"return_hdrs"`
+		PathFmt      string `json:"path_fmt"`
+		Type         string `json:"type"`
+	} `json:"data"`
+}
+
+type HaproxyHttpRequestRules struct {
+	Version int `json:"_version"`
+	Data    []struct {
+		Cond       string  `json:"cond"`
+		CondTest   string  `json:"cond_test"`
+		HdrFormat  string  `json:"hdr_format"`
+		HdrName    string  `json:"hdr_name"`
+		Index      int     `json:"index"`
+		Type       string  `json:"type"`
+		ReturnHdrs *string `json:"return_hdrs"`
+		PathFmt    *string `json:"path_fmt"`
+	} `json:"data"`
+}
+
+type HaproxyAddBackend struct {
+	AdvCheck string `json:"adv_check"`
+	Balance  struct {
+		Algorithm string `json:"algorithm"`
+	} `json:"balance"`
+	Forwardfor struct {
+		Enabled string `json:"enabled"`
+	} `json:"forwardfor"`
+	HttpchkParams struct {
+		Method  string `json:"method"`
+		URI     string `json:"uri"`
+		Version string `json:"version"`
+	} `json:"httpchk_params"`
+	Mode string `json:"mode"`
+	Name string `json:"name"`
+}
+
+type HaproxyAddFrontend struct {
+	DefaultBackend     string `json:"default_backend"`
+	HTTPConnectionMode string `json:"http_connection_mode"`
+	Maxconn            int    `json:"maxconn"`
+	Mode               string `json:"mode"`
+	Name               string `json:"name"`
+}
+
+type HaproxyAddAcl struct {
+	AclName   string `json:"acl_name"`
+	Criterion string `json:"criterion"`
+	Index     int    `json:"index"`
+	Value     string `json:"value"`
+}
+
+type HaproxyAddServer struct {
+	Address string `json:"address"`
+	Check   string `json:"check"`
+	Name    string `json:"name"`
+	Port    int    `json:"port"`
+	Weight  int    `json:"weight"`
+}
+
+type HaproxyAddHttpRequestRule struct {
+	Cond      string `json:"cond"`
+	CondTest  string `json:"cond_test"`
+	HdrFormat string `json:"hdr_format"`
+	HdrName   string `json:"hdr_name"`
+	Index     int    `json:"index"`
+	Type      string `json:"type"`
+}
+
+type HaproxyAddBackendSwitchingRule struct {
+	Cond     string `json:"cond"`
+	CondTest string `json:"cond_test"`
+	Index    int    `json:"index"`
+	Name     string `json:"name"`
+}
+
+type HaproxyCommitTransaction struct {
+	Version int    `json:"_version"`
+	ID      string `json:"id"`
+	Status  string `json:"status"`
+}
+
+type DuplicateCount struct {
+	Name  string `json:"name"`
+	Count int    `json:"count"`
+}
+type HaproxyDuplicateDefinitionsResult struct {
+	Acls      []DuplicateCount `json:"acls"`
+	Backends  []DuplicateCount `json:"backends"`
+	Frontends []DuplicateCount `json:"frontends"`
+	Servers   []DuplicateCount `json:"servers"`
+}
