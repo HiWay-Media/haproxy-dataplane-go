d41cd32e74e4ee55f6468f6d70e21ec81e34cbfe
allan <allan.nava@ies-italia.it>
2022-11-09 17:28:25 +0100

    fix test_client_v2

--- test_client_v2.go
+++ test_client_v2.go
@@ -1,13 +1,15 @@
 package haproxy
 
-
 import (
-    "testing"
-    "log"
-    )
+	"log"
+	"testing"
+)
 
-func TestNewHaproxyClient(t *testing.T){
+func TestNewHaproxyClient(t *testing.T) {
 
-    got := NewHaproxyClient()
-    log.Println("got haproxyClient", got)
+	got, err := NewHaproxyClient("localhost-haproxy", "admin", "password", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("got haproxyClient", got)
 }
